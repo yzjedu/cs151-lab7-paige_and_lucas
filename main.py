77c8024f0e40ceca9f49c5dd37f1a738b277cdc0
lapodowski <lapodowski@loyola.edu>
2024-10-29 19:36:21 -0400

    main.py push 2

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 # your code
-def input_float():
 
 def floor_size():
-    width = int(input('width'))
-    temp = width.replace('.', ' ')
+    width = input('Please enter the width of the room: ')
+    temp = width.replace('.', '')
+
     while not temp.isdigit():
         print("invalid input")
-        width = input('width')
-        temp = width.replace('.', ' ')
-    return float(temp)
-    length = int(input('length'))
-    temp = length.replace('.', ' ')
+        width = input('Please enter the width of the room: ')
+        temp = width.replace('.', '')
+
+    length = input('Please enter the length of the room: ')
+    temp = length.replace('.', '')
+
     while not temp.isdigit():
         print("invalid input")
         length = input('length')
-        temp = length.replace('.', ' ')
-    return float(temp)
-    area = width * length
+        temp = length.replace('.', '')
+    area = float(width) * float(length)
     return area
+
 def floor_style():
-    floor_type = input('flooring type')
+    floor_type = input('Enter the desired floor type (hardwood, carpet, or tile): ')
+    while floor_type not in {'hardwood', 'carpet', 'tile'}:
+        print('invalid input')
+        floor_type = input('Please enter the desired floor type (hardwood, carpet, or tile): ')
     return floor_type
+
 def room_cost():
     floor_type = floor_style()
     area = floor_size()
@@ -29,21 +34,23 @@ def room_cost():
     tile_cost = 4.99
     if floor_type == 'hard':
         cost = hardwood_cost * area
-        return cost
     elif floor_type == 'carpet':
         cost = carpet_cost * area
-        return cost
     else:
         cost = tile_cost * area
-        return cost
+    return cost
 
 
 def main():
     count = 0
-    while count < 5:
+    total_cost = 0
+    print('This program will calculate the total cost to re-do all of your flooring.\n')
+    while count < 2:
         cost = room_cost()
         count += 1
-        print(cost)
+        print(f'Your cost to replace the flooring in room {count} is ${cost:.2f}\n')
+        total_cost += cost
+    print(f'Your total cost for all the rooms is ${total_cost:.2f}')
 
 
 main()
